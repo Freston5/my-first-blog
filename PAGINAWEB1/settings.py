8b74e41b5aa435e5ab3955c5a967db87e647fb65
Freston5 <carlostejedoresquina1@gmail.com>
2017-02-23 23:29:08 -0400

    Corregi error settings.py

--- PAGINAWEB1/settings.py
+++ PAGINAWEB1/settings.py
@@ -15,8 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '9kw40%@f787s@4x59cw8mdn1&=0m*u!-$###!-qg@-_9u4qn$t'
